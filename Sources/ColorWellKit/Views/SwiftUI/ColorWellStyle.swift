ba91d80647d0dcfd45babdb81ed53bf906ff3859
Jordan Baird <jordanbaird.dev@gmail.com>
2023-11-18 06:09:55 -0800

    Add availability attributes to ColorWellStyle

--- Sources/ColorWellKit/Views/SwiftUI/ColorWellStyle.swift
+++ Sources/ColorWellKit/Views/SwiftUI/ColorWellStyle.swift
@@ -8,17 +8,20 @@
 // MARK: - ColorWellStyleConfiguration
 
 /// Values that configure a color well's style.
+@available(macOS 10.15, *)
 public struct _ColorWellStyleConfiguration {
     /// The underlying style of the color well.
     let style: ColorWell.Style
 }
 
+@available(macOS 10.15, *)
 extension _ColorWellStyleConfiguration {
     static var `default`: _ColorWellStyleConfiguration {
         _ColorWellStyleConfiguration(style: ColorWell.BackingStorage.defaultStyle)
     }
 }
 
+@available(macOS 10.15, *)
 extension _ColorWellStyleConfiguration: CustomStringConvertible {
     public var description: String {
         String(describing: style)
@@ -28,6 +31,7 @@ extension _ColorWellStyleConfiguration: CustomStringConvertible {
 // MARK: - ColorWellStyle
 
 /// A type that specifies the appearance and behavior of a color well.
+@available(macOS 10.15, *)
 public protocol ColorWellStyle {
     /// Values that configure the color well's style.
     var _configuration: _ColorWellStyleConfiguration { get }
@@ -39,6 +43,7 @@ public protocol ColorWellStyle {
 /// rectangular control, and toggles the system color panel when clicked.
 ///
 /// You can also use ``default`` to construct this style.
+@available(macOS 10.15, *)
 public struct DefaultColorWellStyle: ColorWellStyle {
     public let _configuration = _ColorWellStyleConfiguration(style: .default)
 
@@ -46,6 +51,7 @@ public struct DefaultColorWellStyle: ColorWellStyle {
     public init() { }
 }
 
+@available(macOS 10.15, *)
 extension ColorWellStyle where Self == DefaultColorWellStyle {
     /// A color well style that displays the color well's color inside of a
     /// rectangular control, and toggles the system color panel when clicked.
@@ -61,6 +67,7 @@ extension ColorWellStyle where Self == DefaultColorWellStyle {
 /// swatch colors when clicked.
 ///
 /// You can also use ``minimal`` to construct this style.
+@available(macOS 10.15, *)
 public struct MinimalColorWellStyle: ColorWellStyle {
     public let _configuration = _ColorWellStyleConfiguration(style: .minimal)
 
@@ -68,6 +75,7 @@ public struct MinimalColorWellStyle: ColorWellStyle {
     public init() { }
 }
 
+@available(macOS 10.15, *)
 extension ColorWellStyle where Self == MinimalColorWellStyle {
     /// A color well style that displays the color well's color inside of a
     /// rectangular control, and shows a popover containing the color well's
@@ -86,6 +94,7 @@ extension ColorWellStyle where Self == MinimalColorWellStyle {
 /// color well's swatch colors.
 ///
 /// You can also use ``expanded`` to construct this style.
+@available(macOS 10.15, *)
 public struct ExpandedColorWellStyle: ColorWellStyle {
     public let _configuration = _ColorWellStyleConfiguration(style: .expanded)
 
@@ -93,6 +102,7 @@ public struct ExpandedColorWellStyle: ColorWellStyle {
     public init() { }
 }
 
+@available(macOS 10.15, *)
 extension ColorWellStyle where Self == ExpandedColorWellStyle {
     /// A color well style that displays the color well's color alongside
     /// a dedicated button that toggles the system color panel.
@@ -112,6 +122,7 @@ extension ColorWellStyle where Self == ExpandedColorWellStyle {
 /// rectangular control, and toggles the system color panel when clicked.
 ///
 /// You can also use ``standard`` to construct this style.
+@available(macOS 10.15, *)
 @available(*, deprecated, renamed: "DefaultColorWellStyle")
 public struct StandardColorWellStyle: ColorWellStyle {
     public let _configuration = _ColorWellStyleConfiguration(style: .standard)
@@ -120,6 +131,7 @@ public struct StandardColorWellStyle: ColorWellStyle {
     public init() { }
 }
 
+@available(macOS 10.15, *)
 @available(*, deprecated)
 extension ColorWellStyle where Self == StandardColorWellStyle {
     /// A color well style that displays the color well's color inside of a
@@ -137,6 +149,7 @@ extension ColorWellStyle where Self == StandardColorWellStyle {
 /// swatch colors when clicked.
 ///
 /// You can also use ``swatches`` to construct this style.
+@available(macOS 10.15, *)
 @available(*, deprecated, renamed: "MinimalColorWellStyle")
 public struct SwatchesColorWellStyle: ColorWellStyle {
     public let _configuration = _ColorWellStyleConfiguration(style: .swatches)
@@ -145,6 +158,7 @@ public struct SwatchesColorWellStyle: ColorWellStyle {
     public init() { }
 }
 
+@available(macOS 10.15, *)
 @available(*, deprecated)
 extension ColorWellStyle where Self == SwatchesColorWellStyle {
     /// A color well style that displays the color well's color inside of a
